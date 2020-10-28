fc856474fe06189f39896b398dc4affcb8fa10f1
jtreeves <jackson.t.reeves@gmail.com>
2020-10-28 11:36:48 -0400

    [js] Add object for Github, then add element from that object to the page

--- github.js
+++ github.js
@@ -1,9 +1,18 @@
+const container = document.querySelector('.container');
+
 fetch('https://api.github.com/users/jtreeves')
 .then(response => {
     return response.json();
 })
 .then(githubData => {
-    console.log(githubData);
+    const jtreevesObj = {
+        bio: githubData.bio,
+        username: githubData.login,
+        name: githubData.name
+    };
+    const newElement = document.createElement('p');
+    newElement.textContent = jtreevesObj.name;
+    container.appendChild(newElement);
 })
 .catch(error => {
     console.log(error);
